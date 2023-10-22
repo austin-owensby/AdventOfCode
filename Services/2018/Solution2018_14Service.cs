7e6b84118dd1f2a2b6f0308f60e7750f7df8f5c5
Austin Owensby <austin_owensby@hotmail.com>
2023-10-22 13:29:05 -0500

    2018 day 14

--- Services/2018/Solution2018_14Service.cs
+++ Services/2018/Solution2018_14Service.cs
@@ -7,27 +7,53 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2018, 14, example);
+            int recipeLimit = int.Parse(lines.First());
 
-            int answer = 0;
+            List<int> recipies = new(){3,7};
+            int index1 = 0;
+            int index2 = 1;
 
-            foreach (string line in lines)
-            {
+            while(recipies.Count < recipeLimit + 10) {
+                int value1 = recipies[index1];
+                int value2 = recipies[index2];
+                int sum = value1 + value2;
+                List<int> newRecipies = sum.ToString().Select(c => int.Parse(c.ToString())).ToList();
+                recipies.AddRange(newRecipies);
 
+                index1 = (index1 + value1 + 1) % recipies.Count;
+                index2 = (index2 + value2 + 1) % recipies.Count;
             }
 
-            return answer.ToString();
+            string answer = string.Concat(recipies.TakeLast(10));
+
+            return answer;
         }
 
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2018, 14, example);
+            string scoreDigits = lines.First();
 
-            int answer = 0;
+            List<int> recipies = new(){3,7};
+            int index1 = 0;
+            int index2 = 1;
 
-            foreach (string line in lines)
-            {
+            bool hasAnswer = false;
 
+            while(!hasAnswer) {
+                int value1 = recipies[index1];
+                int value2 = recipies[index2];
+                int sum = value1 + value2;
+                List<int> newRecipies = sum.ToString().Select(c => int.Parse(c.ToString())).ToList();
+                recipies.AddRange(newRecipies);
+
+                index1 = (index1 + value1 + 1) % recipies.Count;
+                index2 = (index2 + value2 + 1) % recipies.Count;
+
+                hasAnswer = string.Concat(recipies.TakeLast(scoreDigits.Length + 1)).Contains(scoreDigits);
             }
+
+            int answer = string.Concat(recipies).IndexOf(scoreDigits);
 
             return answer.ToString();
         }
