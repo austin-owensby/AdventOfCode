6c113d1778b3fc527de4fe5e7f06215fd16d6b24
Austin Owensby <austin_owensby@hotmail.com>
2023-11-30 23:36:42 -0600

    2023 1

--- Services/2023/Solution2023_01Service.cs
+++ Services/2023/Solution2023_01Service.cs
@@ -9,8 +9,8 @@ namespace AdventOfCode.Services
             int answer = 0;
 
             foreach (string line in lines)
-            {
-
+            {                
+                answer += int.Parse($"{line.First(c => char.IsNumber(c))}{line.Last(c => char.IsNumber(c))}");
             }
 
             return answer.ToString();
@@ -22,9 +22,32 @@ namespace AdventOfCode.Services
 
             int answer = 0;
 
+            string[] numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
             foreach (string line in lines)
             {
+                char minValue = line.First(c => char.IsNumber(c));
+                int minIndex = line.IndexOf(minValue);
+                char maxValue = line.Last(c => char.IsNumber(c));
+                int maxIndex = line.LastIndexOf(maxValue);
 
+                foreach (int i in numbers.Length) {
+                    int newMinIndex = line.IndexOf(numbers[i]);
+                    if (newMinIndex != -1 && newMinIndex < minIndex) {
+                        minIndex = newMinIndex;
+                        minValue = $"{i + 1}"[0];
+                    }
+
+                    int newMaxIndex = line.LastIndexOf(numbers[i]);
+                    if (newMaxIndex > maxIndex) {
+                        maxIndex = newMaxIndex;
+                        maxValue = $"{i + 1}"[0];
+                    }
+                }
+
+                int value = int.Parse($"{minValue}{maxValue}");
+
+                answer += value;
             }
 
             return answer.ToString();
