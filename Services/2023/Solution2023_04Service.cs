e98f76c1b05290a8dc7807f5c3b838fc1c6f75c2
Austin Owensby <austin_owensby@hotmail.com>
2023-12-03 23:30:39 -0600

    2023 4

--- Services/2023/Solution2023_04Service.cs
+++ Services/2023/Solution2023_04Service.cs
@@ -10,7 +10,16 @@ namespace AdventOfCode.Services
 
             foreach (string line in lines)
             {
+                List<string> numbers = line.QuickRegex(@"Card\s+\d+: (.+) \| (.+)");
+                List<int> winningNumbers = numbers.First().Split(" ").Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x)).ToInts();
+                List<int> myNumbers = numbers.Last().Split(" ").Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x)).ToInts();
 
+                int matches = myNumbers.Intersect(winningNumbers).Count();
+
+                if (matches > 0)
+                {
+                    answer += (int)Math.Pow(2, matches - 1);
+                }
             }
 
             return answer.ToString();
@@ -22,10 +31,32 @@ namespace AdventOfCode.Services
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            Dictionary<int, int> cardValues = [];
 
+            foreach (int i in lines.Count) {
+                cardValues[i + 1] = 1;
             }
+
+            for (int cardNumber = lines.Count; cardNumber > 0; cardNumber--) {
+                List<string> numbers = lines[cardNumber - 1].QuickRegex(@"Card\s+\d+: (.+) \| (.+)");
+                List<int> winningNumbers = numbers.First().Split(" ").Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x)).ToInts();
+                List<int> myNumbers = numbers.Last().Split(" ").Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x)).ToInts();
+
+                int matches = myNumbers.Intersect(winningNumbers).Count();
+
+                if (matches > 0)
+                {
+                    int cardValue = 0;
+                    foreach (int i in matches) {
+                        if (cardValues.TryGetValue(cardNumber + i + 1, out int value)) {
+                            cardValue += value;
+                        }
+                    }
+                    cardValues[cardNumber] += cardValue;
+                }
+            }
+
+            answer = cardValues.Sum(card => card.Value);
 
             return answer.ToString();
         }
