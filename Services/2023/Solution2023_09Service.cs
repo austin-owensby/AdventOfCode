6df2d9e12cb2cabe07ed2c96d2da349f4ad9be68
Austin Owensby <austin_owensby@hotmail.com>
2023-12-08 23:34:07 -0600

    2023 9

--- Services/2023/Solution2023_09Service.cs
+++ Services/2023/Solution2023_09Service.cs
@@ -6,11 +6,27 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2023, 9, example);
 
+            List<List<int>> values = lines.Select(line => line.Split(' ').ToInts()).ToList();
+
             int answer = 0;
 
-            foreach (string line in lines)
+            foreach (List<int> value in values)
             {
+                List<List<int>> histories = [value];
 
+                while (!histories.Last().All(v => v == 0)) {
+                    List<int> history = [];
+                    for (int i = 0; i < histories.Last().Count - 1; i++) {
+                        history.Add(histories.Last()[i + 1] - histories.Last()[i]);
+                    }
+                    histories.Add(history);
+                }
+
+                for (int i = histories.Count - 1; i > 0; i--) {
+                    histories[i - 1].Add(histories[i - 1].Last() + histories[i].Last());
+                }
+
+                answer += histories.First().Last();
             }
 
             return answer.ToString();
@@ -20,11 +36,27 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2023, 9, example);
 
+            List<List<int>> values = lines.Select(line => line.Split(' ').ToInts()).ToList();
+
             int answer = 0;
 
-            foreach (string line in lines)
+            foreach (List<int> value in values)
             {
+                List<List<int>> histories = [value];
 
+                while (!histories.Last().All(v => v == 0)) {
+                    List<int> history = [];
+                    for (int i = 0; i < histories.Last().Count - 1; i++) {
+                        history.Add(histories.Last()[i + 1] - histories.Last()[i]);
+                    }
+                    histories.Add(history);
+                }
+
+                for (int i = histories.Count - 1; i > 0; i--) {
+                    histories[i - 1].Insert(0, histories[i - 1].First() - histories[i].First());
+                }
+
+                answer += histories.First().First();
             }
 
             return answer.ToString();
