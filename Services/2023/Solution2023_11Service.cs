d5204dd69c661bda15cf9bd77c39b55fc81a006e
Austin Owensby <austin_owensby@hotmail.com>
2023-12-11 21:13:23 -0600

    2023 11

--- Services/2023/Solution2023_11Service.cs
+++ Services/2023/Solution2023_11Service.cs
@@ -6,11 +6,32 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2023, 11, example);
 
+            // Enumerate the galaxies
+            List<Point> points = [];
+            foreach (int y in lines.Count) {
+                foreach (int x in lines[y].Length) {
+                    if (lines[y][x] == '#') {
+                        points.Add(new(x, y));
+                    }
+                }
+            }
+
+            // Expand the empty rows
+            List<int> emptyRows = lines.FindIndexes(line => line.All(c => c == '.'));
+            List<int> emptyColumns = lines.Pivot().FindIndexes(line => line.All(c => c == '.'));
+
+            foreach (Point point in points) {
+                point.Y -= emptyRows.Count(y => y > point.Y);
+                point.X -= emptyColumns.Count(x => x > point.X);
+            }
+
             int answer = 0;
 
-            foreach (string line in lines)
-            {
-
+            // Sum the distances between each combination
+            foreach (int i in points.Count) {
+                for (int j = i + 1; j < points.Count; j++) {
+                    answer += Math.Abs(points[i].Y - points[j].Y) + Math.Abs(points[i].X - points[j].X);
+                }
             }
 
             return answer.ToString();
@@ -20,11 +41,34 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2023, 11, example);
 
-            int answer = 0;
+            // Enumerate the galaxies
+            List<Point> points = [];
+            foreach (int y in lines.Count) {
+                foreach (int x in lines[y].Length) {
+                    if (lines[y][x] == '#') {
+                        points.Add(new(x, y));
+                    }
+                }
+            }
 
-            foreach (string line in lines)
-            {
+            // Expand the empty rows
+            List<int> emptyRows = lines.FindIndexes(line => line.All(c => c == '.'));
+            List<int> emptyColumns = lines.Pivot().FindIndexes(line => line.All(c => c == '.'));
 
+            int extraRows = 1000000 - 1;
+
+            foreach (Point point in points) {
+                point.Y -= extraRows * emptyRows.Count(y => y > point.Y);
+                point.X -= extraRows * emptyColumns.Count(x => x > point.X);
+            }
+
+            long answer = 0;
+
+            // Sum the distances between each combination
+            foreach (int i in points.Count) {
+                for (int j = i + 1; j < points.Count; j++) {
+                    answer += Math.Abs(points[i].Y - points[j].Y) + Math.Abs(points[i].X - points[j].X);
+                }
             }
 
             return answer.ToString();
