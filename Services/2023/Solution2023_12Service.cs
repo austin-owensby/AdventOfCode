f8f15708807f06f18669a1c2134509f5930a766c
Austin Owensby <austin_owensby@hotmail.com>
2023-12-12 00:05:10 -0600

    2023 12 part 1

--- Services/2023/Solution2023_12Service.cs
+++ Services/2023/Solution2023_12Service.cs
@@ -10,7 +10,48 @@ namespace AdventOfCode.Services
 
             foreach (string line in lines)
             {
+                string[] parts = line.Split(' ');
+                List<int> values = parts.Last().Split(',').ToInts();
+                List<char> springs = parts.First().ToList();
 
+                // Consider each combination
+                int mysterySpringCount = springs.Count(s => s == '?');
+                int combinations = (int)Math.Pow(2, mysterySpringCount);
+                foreach (int i in combinations) {
+                    // Generate an the option based on the combination
+                    List<bool> combination = Convert.ToString(i, 2).PadLeft(mysterySpringCount, '0').ToList().Select(c => c == '1').ToList();
+                    
+                    int unknownSpringIndex = 0;
+                    List<char> option = [];
+                    foreach (char spring in springs) {
+                        if (spring == '?') {
+                            option.Add(combination[unknownSpringIndex] ? '#' : '.');
+                            unknownSpringIndex++;
+                        }
+                        else {
+                            option.Add(spring);
+                        }
+                    }
+
+                    // Check if this is a valid option
+                    List<int> groups = option.ChunkByExclusive(c => c == '.').Select(x => x.Count).ToList();
+                    bool validOption = true;
+                    if (groups.Count == values.Count) {
+                        foreach (int j in groups.Count) {
+                            if (groups[j] != values[j]) {
+                                validOption = false;
+                                break;
+                            }
+                        }
+                    }
+                    else {
+                        validOption = false;
+                    }
+
+                    if (validOption) {
+                        answer++;
+                    }
+                }
             }
 
             return answer.ToString();
