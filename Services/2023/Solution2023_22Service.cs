6d04b6aabb3513709fa3d22911318416c895fc0e
Austin Owensby <austin_owensby@hotmail.com>
2023-12-22 01:13:33 -0600

    2023 22

--- Services/2023/Solution2023_22Service.cs
+++ Services/2023/Solution2023_22Service.cs
@@ -5,12 +5,73 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2023, 22, example);
+            List<List<int>> bricks = lines.Select(line => line.QuickRegex(@"(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)").ToInts()).ToList();
+
+            int maxZ = bricks.Max(brick => brick[5]);
 
             int answer = 0;
 
-            foreach (string line in lines)
+            // Move the bricks starting from the bottom
+            for (int z = 2; z <= maxZ; z++)
             {
+                List<List<int>> bricksToMove = bricks.Where(brick => brick[2] == z).ToList();
 
+                foreach (List<int> brickToMove in bricksToMove) {
+                    bool canDrop = true;
+                    // Keep dropping the brick while we're able
+                    while (canDrop) {
+                        foreach (List<int> innerBrick in bricks.Where(brick => brick[5] == brickToMove[2] - 1)) {
+                            // Check for a potential overlap on each brick
+                            if (brickToMove[0] <= innerBrick[3] && brickToMove[1] <= innerBrick[4] && innerBrick[0] <= brickToMove[3] && innerBrick[1] <= brickToMove[4]) {
+                                canDrop = false;
+                                break;
+                            }
+                        }
+
+                        if (canDrop) {
+                            brickToMove[2]--;
+                            brickToMove[5]--;
+                            canDrop = brickToMove[2] > 1;
+                        }
+                    }
+                }
+            }
+
+            // Test if we can remove each brick
+            foreach (int i in bricks.Count) {
+                List<List<int>> testBricks = bricks.Where((brick, index) => index != i).ToList();
+
+                int z = bricks[i][5] + 1;
+                List<List<int>> bricksToMove = testBricks.Where(brick => brick[2] == z).ToList();
+
+                bool safe = true;
+
+                foreach (List<int> brickToMove in bricksToMove) {
+                    bool canDrop = true;
+                    // Keep dropping the brick while we're able
+                    while (canDrop) {
+                        foreach (List<int> innerBrick in testBricks.Where(brick => brick[5] == brickToMove[2] - 1)) {
+                            // Check for a potential overlap on each brick
+                            if (brickToMove[0] <= innerBrick[3] && brickToMove[1] <= innerBrick[4] && innerBrick[0] <= brickToMove[3] && innerBrick[1] <= brickToMove[4]) {
+                                canDrop = false;
+                                break;
+                            }
+                        }
+
+                        if (canDrop) {
+                            safe = false;
+                            break;
+                        }
+                    }
+
+                    if (!safe){
+                        break;
+                    }
+                }
+
+                if (safe) {
+                    answer++;
+                }
             }
 
             return answer.ToString();
@@ -19,12 +80,74 @@ namespace AdventOfCode.Services
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2023, 22, example);
+            List<List<int>> bricks = lines.Select(line => line.QuickRegex(@"(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)").ToInts()).ToList();
 
-            int answer = 0;
+            int maxZ = bricks.Max(brick => brick[5]);
 
-            foreach (string line in lines)
+            long answer = 0;
+
+            // Move the bricks starting from the bottom
+            for (int z = 2; z <= maxZ; z++)
             {
+                List<List<int>> bricksToMove = bricks.Where(brick => brick[2] == z).ToList();
 
+                foreach (List<int> brickToMove in bricksToMove) {
+                    bool canDrop = true;
+                    // Keep dropping the brick while we're able
+                    while (canDrop) {
+                        foreach (List<int> innerBrick in bricks.Where(brick => brick[5] == brickToMove[2] - 1)) {
+                            // Check for a potential overlap on each brick
+                            if (brickToMove[0] <= innerBrick[3] && brickToMove[1] <= innerBrick[4] && innerBrick[0] <= brickToMove[3] && innerBrick[1] <= brickToMove[4]) {
+                                canDrop = false;
+                                break;
+                            }
+                        }
+
+                        if (canDrop) {
+                            brickToMove[2]--;
+                            brickToMove[5]--;
+                            canDrop = brickToMove[2] > 1;
+                        }
+                    }
+                }
+            }
+
+            // Test the amount of movement from removing each brick
+            foreach (int i in bricks.Count) {
+                List<int> bricksMoved = [];
+                List<List<int>> testBricks = bricks.Where((brick, index) => index != i).Select(brick => brick.ToList()).ToList();
+
+                for (int z = bricks[i][5] + 1; z <= maxZ; z++)
+                {
+                    List<List<int>> bricksToMove = testBricks.Where(brick => brick[2] == z).ToList();
+
+                    foreach (List<int> brickToMove in bricksToMove) {
+                        bool canDrop = true;
+                        // Keep dropping the brick while we're able
+                        while (canDrop) {
+                            foreach (List<int> innerBrick in testBricks.Where(brick => brick[5] == brickToMove[2] - 1)) {
+                                // Check for a potential overlap on each brick
+                                if (brickToMove[0] <= innerBrick[3] && brickToMove[1] <= innerBrick[4] && innerBrick[0] <= brickToMove[3] && innerBrick[1] <= brickToMove[4]) {
+                                    canDrop = false;
+                                    break;
+                                }
+                            }
+
+                            if (canDrop) {
+                                brickToMove[2]--;
+                                brickToMove[5]--;
+                                canDrop = brickToMove[2] > 1;
+
+                                int brickIndex = testBricks.IndexOf(brickToMove);
+                                if (!bricksMoved.Contains(brickIndex)) {
+                                    bricksMoved.Add(brickIndex);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                answer += bricksMoved.Count;
             }
 
             return answer.ToString();
