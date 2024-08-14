1cd91c5125e1abcfdf82152ca355ab703eb37337
Austin Owensby <austin_owensby@hotmail.com>
2024-08-13 21:08:41 -0500

    2019 day 10 part 1

--- Services/2019/Solution2019_10Service.cs
+++ Services/2019/Solution2019_10Service.cs
@@ -10,9 +10,53 @@ namespace AdventOfCode.Services
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            int height = lines.Count;
+            int width = lines.First().Length;
 
+            // Loop over each row
+            foreach (int ySource in height)
+            {
+                // Only loop over coordinates that contain asteriods 
+                List<int> sourceIndexes = lines[ySource].FindIndexes(c => c == '#');
+
+                foreach (int xSource in sourceIndexes) {
+                    int detectedAsteroids = 0;
+
+                    // Iterate over each possible asteroid location
+                    foreach (int yTarget in height) {
+                        // Only loop over coordinates that contain asteriods 
+                        List<int> indexes = lines[yTarget].FindIndexes(c => c == '#');
+
+                        foreach (int xTarget in indexes) {
+                            if (yTarget == ySource && xTarget == xSource) {
+                                // Ignore ourselves
+                                continue;
+                            }
+
+                            int ySlope = yTarget - ySource;
+                            int xSlope = xTarget - xSource;
+
+                            int factor = Math.Abs(Utility.GCF(xSlope, ySlope));
+                            xSlope /= factor;
+                            ySlope /= factor;
+
+                            // Follow the slope from the target asteroid to the source asteroid and check for interuptions
+                            bool interuption = false;
+                            for (int i = factor - 1; i > 0; i--) {
+                                if (lines[yTarget - ySlope * i][xTarget - xSlope * i] == '#') {
+                                    interuption = true;
+                                    break;
+                                }
+                            }
+
+                            if (!interuption) {
+                                detectedAsteroids++;
+                            }
+                        }
+                    }
+
+                    answer = Math.Max(answer, detectedAsteroids);
+                }
             }
 
             return answer.ToString();
@@ -22,11 +66,71 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2019, 10, example);
 
+            int height = lines.Count;
+            int width = lines.First().Length;
+
+            int bestDetectedAsteroids = 0;
+            int bestX = 0;
+            int bestY = 0;
+
+            // Loop over each row
+            foreach (int ySource in height)
+            {
+                // Only loop over coordinates that contain asteriods 
+                List<int> sourceIndexes = lines[ySource].FindIndexes(c => c == '#');
+
+                foreach (int xSource in sourceIndexes) {
+                    int detectedAsteroids = 0;
+
+                    // Iterate over each possible asteroid location
+                    foreach (int yTarget in height) {
+                        // Only loop over coordinates that contain asteriods 
+                        List<int> indexes = lines[yTarget].FindIndexes(c => c == '#');
+
+                        foreach (int xTarget in indexes) {
+                            if (yTarget == ySource && xTarget == xSource) {
+                                // Ignore ourselves
+                                continue;
+                            }
+
+                            int ySlope = yTarget - ySource;
+                            int xSlope = xTarget - xSource;
+
+                            int factor = Math.Abs(Utility.GCF(xSlope, ySlope));
+                            xSlope /= factor;
+                            ySlope /= factor;
+
+                            // Follow the slope from the target asteroid to the source asteroid and check for interuptions
+                            bool interuption = false;
+                            for (int i = factor - 1; i > 0; i--) {
+                                if (lines[yTarget - ySlope * i][xTarget - xSlope * i] == '#') {
+                                    interuption = true;
+                                    break;
+                                }
+                            }
+
+                            if (!interuption) {
+                                detectedAsteroids++;
+                            }
+                        }
+                    }
+
+                    if (detectedAsteroids > bestDetectedAsteroids)
+                    {
+                        bestDetectedAsteroids = detectedAsteroids;
+                        bestX = xSource;
+                        bestY = ySource;
+                    }
+                }
+            }
+
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            // Now that we have the best starting point, vaporize asteroids in order
+            int vaporizedCount = 0;
 
+            while (vaporizedCount < 200) {
+                
             }
 
             return answer.ToString();
