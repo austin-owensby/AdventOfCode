0d63e9395de3b97aa75295a96535ee16314feb69
Austin Owensby <austin_owensby@hotmail.com>
2024-12-04 23:20:11 -0600

    2024 day 5

--- Services/2024/Solution2024_05Service.cs
+++ Services/2024/Solution2024_05Service.cs
@@ -8,11 +8,29 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2024, 5, example);
 
+            List<(int,int)> rules = lines.TakeWhile(l => !string.IsNullOrEmpty(l)).Select(l => l.Split('|')).Select(l => (int.Parse(l[0]),int.Parse(l[1]))).ToList();
+            List<List<int>> updates = lines.SkipWhile(l => !string.IsNullOrEmpty(l)).Skip(1).Select(l => l.Split(',').ToInts()).ToList();
+
             int answer = 0;
 
-            foreach (string line in lines)
+            foreach (List<int> update in updates)
             {
+                bool followsRules = true;
+                foreach ((int, int) rule in rules)
+                {
+                    int index1 = update.IndexOf(rule.Item1);
+                    int index2 = update.IndexOf(rule.Item2);
 
+                    if (index1 != -1 && index2 != -1 && index1 > index2) {
+                        followsRules = false;
+                        break;
+                    }
+                }
+
+                if (followsRules)
+                {
+                    answer += update[(update.Count - 1) / 2];
+                }
             }
 
             return answer.ToString();
@@ -22,11 +40,50 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2024, 5, example);
 
+            List<(int,int)> rules = lines.TakeWhile(l => !string.IsNullOrEmpty(l)).Select(l => l.Split('|')).Select(l => (int.Parse(l[0]),int.Parse(l[1]))).ToList();
+            List<List<int>> updates = lines.SkipWhile(l => !string.IsNullOrEmpty(l)).Skip(1).Select(l => l.Split(',').ToInts()).ToList();
+
             int answer = 0;
 
-            foreach (string line in lines)
-            {
 
+            foreach (List<int> update in updates)
+            {
+                bool followsRules = true;
+                foreach ((int, int) rule in rules)
+                {
+                    int index1 = update.IndexOf(rule.Item1);
+                    int index2 = update.IndexOf(rule.Item2);
+
+                    if (index1 != -1 && index2 != -1 && index1 > index2) {
+                        followsRules = false;
+                        break;
+                    }
+                }
+
+                if (!followsRules)
+                {
+                    // Fix the order
+                    do
+                    {
+                        followsRules = true;
+                        foreach ((int, int) rule in rules)
+                        {
+                            int index1 = update.IndexOf(rule.Item1);
+                            int index2 = update.IndexOf(rule.Item2);
+
+                            if (index1 != -1 && index2 != -1 && index1 > index2) {
+                                followsRules = false;
+                                // Fix the bad rule and try again
+                                update[index1] = rule.Item2;
+                                update[index2] = rule.Item1;
+                                break;
+                            }
+                        }
+                    }
+                    while(followsRules == false);
+
+                    answer += update[(update.Count - 1) / 2];
+                }
             }
 
             return answer.ToString();
