2df126a9e51cd74881cb62d46b76b3248d153b39
Austin Owensby <austin_owensby@hotmail.com>
2024-12-07 00:13:56 -0600

    Strange 2024 day 7 solution

--- Services/2024/Solution2024_07Service.cs
+++ Services/2024/Solution2024_07Service.cs
@@ -7,26 +7,108 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 7, example);
+            List<(long, List<long>)> equations = lines.Select(l => l.Split(": ")).Select(p => (long.Parse(p[0]), p[1].Split(" ").ToLongs())).ToList();
 
-            int answer = 0;
+            long answer = 0;
 
-            foreach (string line in lines)
+            foreach ((long, List<long>) equation in equations)
             {
+                // TODO, is there a better way to do this?
+                int flags = (int)Math.Pow(2, equation.Item2.Count) - 1;
+                while (flags > 0) {
+                    string flagsString = Convert.ToString(flags, 2).PadLeft(equation.Item2.Count, '0');
 
+                    long result = 0;
+
+                    foreach (int i in equation.Item2.Count)
+                    {
+                        if (i == 0) {
+                            result += equation.Item2[i];
+                        }
+                        else if (flagsString[i] == '0') {
+                            result += equation.Item2[i];
+                        }
+                        else {
+                            result *= equation.Item2[i];
+                        }
+
+                        if (result > equation.Item1) {
+                            break;
+                        }
+                    }
+
+                    if (result == equation.Item1) {
+                        answer += result;
+                        flags = 0;
+                    }
+
+                    flags--;
+                }
             }
-
+            
             return answer.ToString();
         }
 
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 7, example);
+            List<(long, List<long>)> equations = lines.Select(l => l.Split(": ")).Select(p => (long.Parse(p[0]), p[1].Split(" ").ToLongs())).ToList();
 
-            int answer = 0;
+            long answer = 0;
 
-            foreach (string line in lines)
+            foreach ((long, List<long>) equation in equations)
             {
+                int flags = (int)Math.Pow(3, equation.Item2.Count) - 1;
+                while (flags > 0) {
+                    // Can't convert to base 3 like we can base 2
+                    string flagsString = "";
+                    int remainder = flags;
+                    foreach (int i in equation.Item2.Count) {
+                        int magnitude = (int)Math.Pow(3, equation.Item2.Count - 1 - i);
 
+                        if (remainder >= magnitude * 2) {
+                            flagsString += "2";
+                            remainder -= magnitude * 2;
+                        }
+                        else if (remainder >= magnitude) {
+                            flagsString += "1";
+                            remainder -= magnitude;
+                        }
+                        else {
+                            flagsString += "0";
+                        }
+                    }
+
+                    long result = 0;
+
+                    foreach (int i in equation.Item2.Count)
+                    {
+                        if (i == 0) {
+                            result += equation.Item2[i];
+                        }
+                        else if (flagsString[i] == '0') {
+                            result += equation.Item2[i];
+                        }
+                        else if (flagsString[i] == '1')
+                        {
+                            result *= equation.Item2[i];
+                        }
+                        else {
+                            result = long.Parse($"{result}{equation.Item2[i]}");
+                        }
+
+                        if (result > equation.Item1) {
+                            break;
+                        }
+                    }
+
+                    if (result == equation.Item1) {
+                        answer += result;
+                        flags = 0;
+                    }
+
+                    flags--;
+                }
             }
 
             return answer.ToString();
