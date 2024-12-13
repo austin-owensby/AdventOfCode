e8a24fcab02da79f7991703d79b94b5832114bca
Austin Owensby <austin_owensby@hotmail.com>
2024-12-13 00:35:02 -0600

    2024 day 13

--- Shared/Services/2024/Solution2024_13Service.cs
+++ Shared/Services/2024/Solution2024_13Service.cs
@@ -6,30 +6,57 @@ namespace AdventOfCode.Services
     {
         public string FirstHalf(bool example)
         {
-            List<string> lines = Utility.GetInputLines(2024, 13, example);
+                List<string> lines = Utility.GetInputLines(2024, 13, example);
+                List<List<string>> machines = lines.ChunkByExclusive(l => string.IsNullOrWhiteSpace(l));
 
-            int answer = 0;
+                double answer = 0;
 
-            foreach (string line in lines)
-            {
+                foreach (List<string> machine in machines)
+                {
+                    List<double> aValues = Utility.QuickRegex(machine[0], @"Button A: X\+(\d+), Y\+(\d+)").Select(double.Parse).ToList();
+                    List<double> bValues = Utility.QuickRegex(machine[1], @"Button B: X\+(\d+), Y\+(\d+)").Select(double.Parse).ToList();
+                    List<double> goal = Utility.QuickRegex(machine[2], @"Prize: X=(\d+), Y=(\d+)").Select(double.Parse).ToList();
 
-            }
+                    // Math, trust me bro
+                    // TODO, why didn't this work?
+                    // double b = (goal[0] - aValues[0] * goal[1] / aValues[1]) / (bValues[0] - aValues[0] * bValues[1] / aValues[1]);
+                    double b = (aValues[1] * goal[0] - aValues[0] * goal[1]) / (aValues[1] * bValues[0] - aValues[0] * bValues[1]);
+                    double a = (goal[0] - bValues[0] * b)/aValues[0];
 
-            return answer.ToString();
+                    // Check for valid solution
+                    if (a >= 0 && b >= 0 && a % 1 == 0 && b % 1 == 0) {
+                        answer += a * 3 + b;
+                    }
+                }
+
+                return answer.ToString();
         }
 
+        // 10000000000000
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 13, example);
+                List<List<string>> machines = lines.ChunkByExclusive(l => string.IsNullOrWhiteSpace(l));
 
-            int answer = 0;
+                double answer = 0;
 
-            foreach (string line in lines)
-            {
+                foreach (List<string> machine in machines)
+                {
+                    List<double> aValues = Utility.QuickRegex(machine[0], @"Button A: X\+(\d+), Y\+(\d+)").Select(double.Parse).ToList();
+                    List<double> bValues = Utility.QuickRegex(machine[1], @"Button B: X\+(\d+), Y\+(\d+)").Select(double.Parse).ToList();
+                    List<double> goal = Utility.QuickRegex(machine[2], @"Prize: X=(\d+), Y=(\d+)").Select(x => double.Parse(x) + 10000000000000).ToList();
 
-            }
+                    // Math, trust me bro
+                    double b = (aValues[1] * goal[0] - aValues[0] * goal[1]) / (aValues[1] * bValues[0] - aValues[0] * bValues[1]);
+                    double a = (goal[0] - bValues[0] * b)/aValues[0];
 
-            return answer.ToString();
+                    // Check for valid solution
+                    if (a >= 0 && b >= 0 && a % 1 == 0 && b % 1 == 0) {
+                        answer += a * 3 + b;
+                    }
+                }
+
+                return answer.ToString();
         }
     }
 }
