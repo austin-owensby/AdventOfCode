babead9f71a2e0ea13ffe732b5229317ae6a3996
Austin Owensby <austin_owensby@hotmail.com>
2024-12-12 18:15:27 -0600

    2024 day 12

--- Shared/Services/2024/Solution2024_12Service.cs
+++ Shared/Services/2024/Solution2024_12Service.cs
@@ -51,10 +51,11 @@ namespace AdventOfCode.Services
             return answer.ToString();
         }
 
-        private class NeighborPoint(double x, double y)
+        private class NeighborPoint(double x, double y, int direction)
         {
             public double X { get; set; } = x;
             public double Y { get; set; } = y;
+            public int Direction { get; set; } = direction;
         }
 
         public string SecondHalf(bool example)
@@ -73,7 +74,6 @@ namespace AdventOfCode.Services
                     if (!visitedPoints.Any(p => p.X == x && p.Y == y)) {
                         // Get the score for this new region
                         List<NeighborPoint> perimeterNeighbors = [];
-                        List<List<NeighborPoint>> distinctSides = [];
                         int area = 0;
                         char letter = grid[y][x];
                         Queue<Point> queue = new();
@@ -93,52 +93,20 @@ namespace AdventOfCode.Services
 
                             if (neighbors.Count < 4) {
                                 if (point.X == 0) {
-                                    NeighborPoint neighbor = new(-0.5, point.Y);
-                                    List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.X == neighbor.X && Math.Abs(p.Y - neighbor.Y) == 1));
-
-                                    if (side == null) {
-                                        side = [neighbor];
-                                        distinctSides.Add(side);
-                                    }
-                                    else {
-                                        side.Add(neighbor);
-                                    }
+                                    NeighborPoint neighbor = new(-0.5, point.Y, 1);
+                                    perimeterNeighbors.Add(neighbor);
                                 }
                                 if (point.Y == 0) {
-                                    NeighborPoint neighbor = new(point.X, -0.5);
-                                    List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.Y == neighbor.Y && Math.Abs(p.X - neighbor.X) == 1));
-
-                                    if (side == null) {
-                                        side = [neighbor];
-                                        distinctSides.Add(side);
-                                    }
-                                    else {
-                                        side.Add(neighbor);
-                                    }
+                                    NeighborPoint neighbor = new(point.X, -0.5, 2);
+                                    perimeterNeighbors.Add(neighbor);
                                 }
                                 if (point.X == grid[0].Count - 1) {
-                                    NeighborPoint neighbor = new(grid[0].Count - 0.5, point.Y);
-                                    List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.X == neighbor.X && Math.Abs(p.Y - neighbor.Y) == 1));
-
-                                    if (side == null) {
-                                        side = [neighbor];
-                                        distinctSides.Add(side);
-                                    }
-                                    else {
-                                        side.Add(neighbor);
-                                    }
+                                    NeighborPoint neighbor = new(grid[0].Count - 0.5, point.Y, 3);
+                                    perimeterNeighbors.Add(neighbor);
                                 }
                                 if (point.Y == grid.Count - 1) {
-                                    NeighborPoint neighbor = new(point.X, grid.Count - 0.5);
-                                    List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.Y == neighbor.Y && Math.Abs(p.X - neighbor.X) == 1));
-
-                                    if (side == null) {
-                                        side = [neighbor];
-                                        distinctSides.Add(side);
-                                    }
-                                    else {
-                                        side.Add(neighbor);
-                                    }
+                                    NeighborPoint neighbor = new(point.X, grid.Count - 0.5, 4);
+                                    perimeterNeighbors.Add(neighbor);
                                 }
                             }
 
@@ -149,56 +117,72 @@ namespace AdventOfCode.Services
                                 else {
                                     if (point.X == neighborPoint.X) {
                                         if (point.Y > neighborPoint.Y) {
-                                            NeighborPoint neighbor = new(neighborPoint.X, neighborPoint.Y + 0.5);
-                                            List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.Y == neighbor.Y && Math.Abs(p.X - neighbor.X) == 1));
-
-                                            if (side == null) {
-                                                side = [neighbor];
-                                                distinctSides.Add(side);
-                                            }
-                                            else {
-                                                side.Add(neighbor);
-                                            }
+                                            NeighborPoint neighbor = new(neighborPoint.X, neighborPoint.Y + 0.5, 4);
+                                            perimeterNeighbors.Add(neighbor);
                                         }
                                         if (point.Y < neighborPoint.Y) {
-                                            NeighborPoint neighbor = new(neighborPoint.X, neighborPoint.Y - 0.5);
-                                            List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.Y == neighbor.Y && Math.Abs(p.X - neighbor.X) == 1));
-
-                                            if (side == null) {
-                                                side = [neighbor];
-                                                distinctSides.Add(side);
-                                            }
-                                            else {
-                                                side.Add(neighbor);
-                                            }
+                                            NeighborPoint neighbor = new(neighborPoint.X, neighborPoint.Y - 0.5, 2);
+                                            perimeterNeighbors.Add(neighbor);
                                         }
                                     }
                                     if (point.Y == neighborPoint.Y) {
                                         if (point.X > neighborPoint.X) {
-                                            NeighborPoint neighbor = new(neighborPoint.X + 0.5, neighborPoint.Y);
-                                            List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.X == neighbor.X && Math.Abs(p.Y - neighbor.Y) == 1));
-
-                                            if (side == null) {
-                                                side = [neighbor];
-                                                distinctSides.Add(side);
-                                            }
-                                            else {
-                                                side.Add(neighbor);
-                                            }
+                                            NeighborPoint neighbor = new(neighborPoint.X + 0.5, neighborPoint.Y, 3);
+                                            perimeterNeighbors.Add(neighbor);
                                         }
                                         if (point.X < neighborPoint.X) {
-                                            NeighborPoint neighbor = new(neighborPoint.X - 0.5, neighborPoint.Y);
-                                            List<NeighborPoint>? side = distinctSides.FirstOrDefault(side => side.Any(p => p.X == neighbor.X && Math.Abs(p.Y - neighbor.Y) == 1));
-
-                                            if (side == null) {
-                                                side = [neighbor];
-                                                distinctSides.Add(side);
-                                            }
-                                            else {
-                                                side.Add(neighbor);
-                                            }
+                                            NeighborPoint neighbor = new(neighborPoint.X - 0.5, neighborPoint.Y, 1);
+                                            perimeterNeighbors.Add(neighbor);
                                         }
                                     }
+                                }
+                            }
+                        }
+
+                        // Based on the neighbors, calculate the distinct side
+                        List<List<NeighborPoint>> distinctSides = [];
+                        foreach (NeighborPoint neighbor in perimeterNeighbors) {
+                            // Check if we've already accounted for this neighbor
+                            if (!distinctSides.SelectMany(s => s).Any(p => p.X == neighbor.X && p.Y == neighbor.Y && p.Direction == neighbor.Direction)) {
+                                // Get the point that are aligned with this point
+                                List<NeighborPoint> potentialSide = perimeterNeighbors.Where(p => p.Direction == neighbor.Direction && (neighbor.X % 1 == 0 && p.Y == neighbor.Y || neighbor.Y % 1 == 0 && p.X == neighbor.X)).ToList();
+
+                                // For the simple case of 1, instantly return the side
+                                if (potentialSide.Count == 1) {
+                                    distinctSides.Add(potentialSide);
+                                    continue;
+                                }
+
+                                // Check if this side is vertical or horizontal
+                                if (potentialSide[0].X == potentialSide[1].X) {
+                                    potentialSide = potentialSide.OrderBy(p => p.Y).ToList();
+                                    // Loop over the points, if there is a gap, detect a new side
+                                    List<NeighborPoint> side = [];
+                                    foreach (int i in potentialSide.Count - 1) {
+                                        side.Add(potentialSide[i]);
+
+                                        if (potentialSide[i].Y + 1 != potentialSide[i + 1].Y) {
+                                            distinctSides.Add(side);
+                                            side = [];
+                                        }
+                                    }
+                                    side.Add(potentialSide.Last());
+                                    distinctSides.Add(side);
+                                }
+                                else {
+                                    potentialSide = potentialSide.OrderBy(p => p.X).ToList();
+                                    // Loop over the points, if there is a gap, detect a new side
+                                    List<NeighborPoint> side = [];
+                                    foreach (int i in potentialSide.Count - 1) {
+                                        side.Add(potentialSide[i]);
+
+                                        if (potentialSide[i].X + 1 != potentialSide[i + 1].X) {
+                                            distinctSides.Add(side);
+                                            side = [];
+                                        }
+                                    }
+                                    side.Add(potentialSide.Last());
+                                    distinctSides.Add(side);
                                 }
                             }
                         }
