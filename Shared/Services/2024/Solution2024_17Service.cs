c2da7740f42cdbdb152914d0bad46604720581e7
Austin Owensby <austin_owensby@hotmail.com>
2024-12-17 01:41:33 -0600

    2024 day 17 partial

--- Shared/Services/2024/Solution2024_17Service.cs
+++ Shared/Services/2024/Solution2024_17Service.cs
@@ -7,13 +7,66 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 17, example);
+            int regA = int.Parse(lines[0].Split(": ")[1]);
+            int regB = int.Parse(lines[1].Split(": ")[1]);
+            int regC = int.Parse(lines[2].Split(": ")[1]);
+            List<int> program = lines.Last().Split(": ")[1].Split(",").ToInts();
 
-            int answer = 0;
+            int instruction_pointer = 0;
 
-            foreach (string line in lines)
-            {
+            List<int> outputs = [];
 
+            while (true) {
+                if (instruction_pointer >= program.Count) {
+                    break;
+                }
+
+                int opCode = program[instruction_pointer];
+                int literal_operand = program[instruction_pointer + 1];
+
+                int combo_operand = literal_operand switch {
+                    4 => regA,
+                    5 => regB,
+                    6 => regC,
+                    _ => literal_operand
+                };
+
+                switch (opCode) {
+                    case 0:
+                        regA = (int)(regA / Math.Pow(2, combo_operand));
+                        instruction_pointer += 2;
+                        break;
+                    case 1:
+                        regB ^= literal_operand;
+                        instruction_pointer += 2;
+                        break;
+                    case 2:
+                        regB = combo_operand % 8;
+                        instruction_pointer += 2;
+                        break;
+                    case 3:
+                        instruction_pointer = regA == 0 ? instruction_pointer + 2 : literal_operand;
+                        break;
+                    case 4:
+                        regB ^= regC;
+                        instruction_pointer += 2;
+                        break;
+                    case 5:
+                        outputs.Add(combo_operand % 8);
+                        instruction_pointer += 2;
+                        break;
+                    case 6:
+                        regB = (int)(regA / Math.Pow(2, combo_operand));
+                        instruction_pointer += 2;
+                        break;
+                    case 7:
+                        regC = (int)(regA / Math.Pow(2, combo_operand));
+                        instruction_pointer += 2;
+                        break;
+                }
             }
+
+            string answer = string.Join(",", outputs);
 
             return answer.ToString();
         }
@@ -22,13 +75,83 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2024, 17, example);
 
-            int answer = 0;
+            long answer = 0;
 
-            foreach (string line in lines)
-            {
+            long iteration = 0;
 
+            while (answer == 0) {
+                long regA = iteration;
+                long regB = long.Parse(lines[1].Split(": ")[1]);
+                long regC = long.Parse(lines[2].Split(": ")[1]);
+                List<long> program = lines.Last().Split(": ")[1].Split(",").ToLongs();
+
+                int instruction_pointer = 0;
+
+                List<long> outputs = [];
+
+                bool keepLooping = true;
+                while (keepLooping) {
+                    if (instruction_pointer >= program.Count) {
+                        break;
+                    }
+
+                    long opCode = program[instruction_pointer];
+                    long literal_operand = program[instruction_pointer + 1];
+
+                    long combo_operand = literal_operand switch {
+                        4 => regA,
+                        5 => regB,
+                        6 => regC,
+                        _ => literal_operand
+                    };                    
+
+                    switch (opCode) {
+                        case 0:
+                            regA = (long)(regA / Math.Pow(2, combo_operand));
+                            instruction_pointer += 2;
+                            break;
+                        case 1:
+                            regB ^= literal_operand;
+                            instruction_pointer += 2;
+                            break;
+                        case 2:
+                            regB = combo_operand % 8;
+                            instruction_pointer += 2;
+                            break;
+                        case 3:
+                            instruction_pointer = regA == 0 ? instruction_pointer + 2 : (int)literal_operand;
+                            break;
+                        case 4:
+                            regB ^= regC;
+                            instruction_pointer += 2;
+                            break;
+                        case 5:
+                            // Check if we match the program so far
+                            if (program[outputs.Count] != combo_operand % 8) {
+                                keepLooping = false;
+                                break;
+                            }
+
+                            outputs.Add(combo_operand % 8);
+                            instruction_pointer += 2;
+                            break;
+                        case 6:
+                            regB = (long)(regA / Math.Pow(2, combo_operand));
+                            instruction_pointer += 2;
+                            break;
+                        case 7:
+                            regC = (long)(regA / Math.Pow(2, combo_operand));
+                            instruction_pointer += 2;
+                            break;
+                    }
+                }
+
+                if (outputs.SequenceEqual(program)) {
+                    answer = iteration;
+                }
+
+                iteration++;
             }
-
             return answer.ToString();
         }
     }
