bb2c6020ae1ca8650668b86b0da8e2d171166ce8
Austin Owensby <austin_owensby@hotmail.com>
2024-12-17 23:32:47 -0600

    2024 day 18

--- Shared/Services/2024/Solution2024_18Service.cs
+++ Shared/Services/2024/Solution2024_18Service.cs
@@ -7,12 +7,51 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 18, example);
+            int steps = example ? 12 : 1024;
+            int dimensions = example ? 6 : 70;
+            List<Point> memory = lines.Take(steps).Select(l => l.Split(",").ToInts()).Select(l => new Point(l[0], l[1])).ToList();
+            List<List<char>> grid = [];
+
+            foreach (int y in dimensions + 1) {
+                List<char> row = [];
+                foreach (int x in dimensions + 1) {
+                    if (memory.Any(m => m.X == x && m.Y == y)) {
+                        row.Add('#');
+                    }
+                    else {
+                        row.Add('.');
+                    }
+                }
+                grid.Add(row);
+            }
+
+            PriorityQueue<Point, int> queue = new();
+            queue.Enqueue(new Point(0,0), dimensions * 2);
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            Dictionary<string, int> history = [];
 
+            while (answer == 0) {
+                Point point = queue.Dequeue();
+                
+                if (history.TryGetValue($"{point.X},{point.Y}", out int value)) {
+                    if (value <= point.Value) {
+                        continue;
+                    }
+                }
+                history[$"{point.X},{point.Y}"] = point.Value;
+
+                if (point.X == dimensions && point.Y == dimensions) {
+                    answer = point.Value;
+                }
+
+                List<Point> validNeighbors = grid.GetNeighbors(point.X, point.Y).Where(p => grid[p.Y][p.X] != '#').ToList();
+
+                foreach (Point validNeighbor in validNeighbors) {
+                    validNeighbor.Value = point.Value + 1;
+                    queue.Enqueue(validNeighbor, dimensions - point.X + dimensions - point.Y + validNeighbor.Value);
+                }
             }
 
             return answer.ToString();
@@ -21,14 +60,67 @@ namespace AdventOfCode.Services
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 18, example);
+            int maxSuccess = example ? 12 : 1024;
+            int minFailure = lines.Count;
+            int dimensions = example ? 6 : 70;
 
-            int answer = 0;
+            string answer = "";
 
-            foreach (string line in lines)
-            {
+            while (minFailure - maxSuccess != 1) {
+                int steps = maxSuccess + (minFailure - maxSuccess) / 2;
+                List<Point> memory = lines.Take(steps).Select(l => l.Split(",").ToInts()).Select(l => new Point(l[0], l[1])).ToList();
+                List<List<char>> grid = [];
 
+                foreach (int y in dimensions + 1) {
+                    List<char> row = [];
+                    foreach (int x in dimensions + 1) {
+                        if (memory.Any(m => m.X == x && m.Y == y)) {
+                            row.Add('#');
+                        }
+                        else {
+                            row.Add('.');
+                        }
+                    }
+                    grid.Add(row);
+                }
+
+                PriorityQueue<Point, int> queue = new();
+                queue.Enqueue(new Point(0,0), dimensions * 2);
+
+                int loopAnswer = 0;
+
+                Dictionary<string, int> history = [];
+
+                while (loopAnswer == 0 && queue.Count > 0) {
+                    Point point = queue.Dequeue();
+                    
+                    if (history.TryGetValue($"{point.X},{point.Y}", out int value)) {
+                        if (value <= point.Value) {
+                            continue;
+                        }
+                    }
+                    history[$"{point.X},{point.Y}"] = point.Value;
+
+                    if (point.X == dimensions && point.Y == dimensions) {
+                        loopAnswer = point.Value;
+                    }
+
+                    List<Point> validNeighbors = grid.GetNeighbors(point.X, point.Y).Where(p => grid[p.Y][p.X] != '#').ToList();
+
+                    foreach (Point validNeighbor in validNeighbors) {
+                        validNeighbor.Value = point.Value + 1;
+                        queue.Enqueue(validNeighbor, dimensions - point.X + dimensions - point.Y + validNeighbor.Value);
+                    }
+                }
+            
+                if (loopAnswer == 0) {
+                    answer = $"{memory.Last().X},{memory.Last().Y}";
+                    minFailure = steps; 
+                }
+                else {
+                    maxSuccess = steps;
+                }
             }
-
             return answer.ToString();
         }
     }
