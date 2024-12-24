c3c68ab2346779a7ce3cd0a4b81a218bb3107530
Austin Owensby <austin_owensby@hotmail.com>
2024-12-23 22:55:54 -0600

    2024 days 23 and 24

--- Shared/Services/2024/Solution2024_22Service.cs
+++ Shared/Services/2024/Solution2024_22Service.cs
@@ -7,12 +7,24 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 22, example);
+            List<long> secretNumbers = lines.ToLongs();
+            int loops = 2000;
+            int pruneValue = 16777216;
 
-            int answer = 0;
+            long answer = 0;
 
-            foreach (string line in lines)
-            {
-
+            foreach (int i in secretNumbers.Count) {
+                long secretNumber = secretNumbers[i];
+                foreach (int loop in loops)
+                {
+                    secretNumber ^= secretNumber * 64;
+                    secretNumber %= pruneValue;
+                    secretNumber ^= secretNumber / 32;
+                    secretNumber %= pruneValue;
+                    secretNumber ^= secretNumber * 2048;
+                    secretNumber %= pruneValue;
+                }
+                answer += secretNumber;
             }
 
             return answer.ToString();
@@ -21,13 +33,44 @@ namespace AdventOfCode.Services
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 22, example);
+            List<long> secretNumbers = lines.ToLongs();
+            int loops = 2000;
+            int pruneValue = 16777216;
 
-            int answer = 0;
+            long answer = 0;
 
-            foreach (string line in lines)
-            {
+            List<Dictionary<(long, long, long, long), long>> sequenceValues = [];
 
+            foreach (int i in secretNumbers.Count) {
+                Dictionary<(long, long, long, long), long> sequenceValueItem = [];
+                List<long> sequenceHistory = [];
+                long secretNumber = secretNumbers[i];
+                foreach (int loop in loops)
+                {
+                    long digit = secretNumber % 10;
+                    secretNumber ^= secretNumber * 64;
+                    secretNumber %= pruneValue;
+                    secretNumber ^= secretNumber / 32;
+                    secretNumber %= pruneValue;
+                    secretNumber ^= secretNumber * 2048;
+                    secretNumber %= pruneValue;
+                    long nextDigit = secretNumber % 10; 
+                    sequenceHistory.Add(nextDigit - digit);
+
+                    sequenceHistory = sequenceHistory.TakeLast(4).ToList();
+
+                    if (sequenceHistory.Count == 4) {
+                        (long, long, long, long) key = (sequenceHistory[0], sequenceHistory[1], sequenceHistory[2], sequenceHistory[3]);
+                        if (!sequenceValueItem.ContainsKey(key)) {
+                            sequenceValueItem[key] = nextDigit;
+                        }
+                    }
+                }
+                sequenceValues.Add(sequenceValueItem);
             }
+
+            List<(long, long, long, long)> keys = sequenceValues.SelectMany(x => x.Keys).Distinct().ToList();
+            answer = keys.Max(key => sequenceValues.Sum(s => s.ContainsKey(key) ? s[key] : 0));
 
             return answer.ToString();
         }
--- Shared/Services/2024/Solution2024_23Service.cs
+++ Shared/Services/2024/Solution2024_23Service.cs
@@ -7,13 +7,30 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 23, example);
+            List<(string, string)> connections = lines.Select(l => l.Split("-")).Select(l => (l[0], l[1])).ToList();
+            List<string> potentialStarts = connections.Select(l => l.Item1).Concat(connections.Select(l => l.Item2)).Where(l => l.StartsWith("t")).Distinct().ToList();
 
-            int answer = 0;
+            List<string> targetConnections = [];
 
-            foreach (string line in lines)
+            foreach (string potentialStart in potentialStarts)
             {
+                List<string> connection1s = connections.Where(c => c.Item1 == potentialStart || c.Item2 == potentialStart).Select(l => l.Item1 == potentialStart ? l.Item2 : l.Item1).ToList();
 
+                for (int i = 0; i < connection1s.Count - 1; i++) {
+                    for (int j = i + 1; j < connection1s.Count; j++) {
+                        if (connections.Any(c => c.Item1 == connection1s[i] && c.Item2 == connection1s[j] || c.Item2 == connection1s[i] && c.Item1 == connection1s[j])) {
+                            List<string> targetConnection = [potentialStart, connection1s[i], connection1s[j]];
+                            string value = string.Join("", targetConnection.Order());
+
+                            if (!targetConnections.Contains(value)) {
+                                targetConnections.Add(value);
+                            }
+                        }
+                    }
+                }
             }
+
+            int answer = targetConnections.Count;
 
             return answer.ToString();
         }
@@ -21,15 +38,47 @@ namespace AdventOfCode.Services
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2024, 23, example);
+            List<(string, string)> connections = lines.Select(l => l.Split("-")).Select(l => (l[0], l[1])).ToList();
+            List<string> potentialStarts = connections.Select(l => l.Item1).Concat(connections.Select(l => l.Item2)).Distinct().ToList();
 
-            int answer = 0;
+            List<List<string>> networks = [];
 
-            foreach (string line in lines)
+            int i = 0;
+
+            foreach (string start in potentialStarts)
             {
+                Console.WriteLine($"{++i}/{potentialStarts.Count}");
+                List<string> startConnections = connections.Where(c => c.Item1 == start || c.Item2 == start).Select(l => l.Item1 == start ? l.Item2 : l.Item1).ToList();
 
+                PriorityQueue<List<string>, int> queue = new();
+
+                foreach (string startConnection in startConnections)
+                {
+                    queue.Enqueue([start, startConnection], -1);
+                }
+
+                while (queue.Count > 0) {
+                    List<string> network = queue.Dequeue();
+
+                    List<string> nextConnections = potentialStarts.Where(s => network.All(n => connections.Any(c => c.Item1 == s && c.Item2 == n || c.Item1 == n && c.Item2 == s))).ToList();
+
+                    if (nextConnections.Count == 0){
+                        networks.Add(network);
+                        continue;
+                    }
+
+                    foreach (string nextConnection in nextConnections) {
+                        List<string> newNetwork = [..network, nextConnection];
+                        if (!networks.Any(n => newNetwork.All(x => n.Contains(x)))) {
+                            queue.Enqueue(newNetwork, -newNetwork.Count);
+                        }
+                    }
+                }
             }
 
-            return answer.ToString();
+            string answer = string.Join(",", networks.MaxBy(n => n.Count)!.Order());
+
+            return answer;
         }
     }
 }
