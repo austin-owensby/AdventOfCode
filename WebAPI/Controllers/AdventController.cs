b43d96c118f413247947fa55a074d0d036465c83
Austin Owensby <austin_owensby@hotmail.com>
2025-11-20 21:27:33 -0600

    Replace magic number 25

--- WebAPI/Controllers/AdventController.cs
+++ WebAPI/Controllers/AdventController.cs
@@ -34,9 +34,17 @@ namespace AdventOfCode.WebAPI.Controllers
                 return BadRequest("You're attempting to submit your answer to AOC while using an example input, this is likely a mistake.");
             }
 
-            if (day == 25 && secondHalf)
+            int puzzles = Globals.NUMBER_OF_PUZZLES;
+
+            if (year >= Globals.NEW_PUZZLE_NUMBER_YEAR)
             {
-                return NotFound("There is no problem for Day 25 part 2, solve all other problems to get the last star.");
+                // Update the number of puzzles starting this year
+                puzzles = Globals.NEW_NUMBER_OF_PUZZLES;
+            }
+
+            if (day == puzzles && secondHalf)
+            {
+                return NotFound($"There is no problem for Day {puzzles} part 2, solve all other problems to get the last star.");
             }
 
             try
