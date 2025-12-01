5f62d56af3551e1506de95781ccdaa05ceb98dd8
Austin Owensby <austin_owensby@hotmail.com>
2025-12-01 07:49:34 -0600

    2025 Day 1

--- Shared/Services/2025/Solution2025_01Service.cs
+++ Shared/Services/2025/Solution2025_01Service.cs
@@ -10,9 +10,18 @@ namespace AdventOfCode.Services
 
             int answer = 0;
 
+            int dial = 50;
+
             foreach (string line in lines)
             {
+                int change = int.Parse(line.Replace("R", "").Replace("L", "-"));
 
+                dial = Utility.Mod(dial + change, 100);
+
+                if (dial == 0)
+                {
+                    answer++;
+                }
             }
 
             return answer.ToString();
@@ -24,10 +33,29 @@ namespace AdventOfCode.Services
 
             int answer = 0;
 
+            int dial = 50;
+
             foreach (string line in lines)
             {
+                int change = int.Parse(line.Replace("R", "").Replace("L", "-"));
+                int step = change < 0 ? -1 : 1;
+
+                for (int i = 0; i < Math.Abs(change); i++)
+                {
+                    dial += step * 1;
+                    dial = Utility.Mod(dial, 100);
+
+                    if (dial == 0)
+                    {
+                        answer++;
+                    }
+                }
+
+                Console.WriteLine($"{dial} {step} {change} {answer}");
 
             }
+
+            // 7276
 
             return answer.ToString();
         }
