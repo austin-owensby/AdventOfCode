45f644518deef6f4bf002f0d39497e31c95c448a
Austin Owensby <austin_owensby@hotmail.com>
2025-12-03 08:02:27 -0600

    2025 day 3

--- Shared/Services/2025/Solution2025_03Service.cs
+++ Shared/Services/2025/Solution2025_03Service.cs
@@ -7,12 +7,31 @@ namespace AdventOfCode.Services
         public string FirstHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2025, 3, example);
+            List<List<int>> grid = lines.ToIntGrid();
 
             int answer = 0;
 
-            foreach (string line in lines)
+            foreach (List<int> bank in grid)
             {
+                int a = bank[0];
+                int b = bank[1];
 
+                for (int i = 2; i < bank.Count; i++)
+                {
+                    int c = bank[i];
+                    if (b > a)
+                    {
+                        a = b;
+                        b = c;
+                    }
+                    else if(c > b)
+                    {
+                        b = c;
+                    }
+                }
+
+                int joltage = a * 10 + b;
+                answer += joltage;
             }
 
             return answer.ToString();
@@ -21,12 +40,43 @@ namespace AdventOfCode.Services
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2025, 3, example);
+            List<List<int>> grid = lines.ToIntGrid();
 
-            int answer = 0;
+            long answer = 0;
 
-            foreach (string line in lines)
+            foreach (List<int> bank in grid)
             {
+                List<int> joltageList = bank.Take(12).ToList();
 
+                for (int i = joltageList.Count; i < bank.Count; i++)
+                {
+                    bool removed = false;
+
+                    for (int j = 0; j < joltageList.Count - 1; j++)
+                    {
+                        if (joltageList[j] < joltageList[j + 1])
+                        {
+                            joltageList.RemoveAt(j);
+                            removed = true;
+                            break;
+                        }
+                    }
+
+                    int c = bank[i];
+
+                    if (removed)
+                    {
+                        joltageList.Add(c);
+                    }
+                    else if (c > joltageList.Last())
+                    {
+                        joltageList.RemoveAt(11);
+                        joltageList.Add(c);
+                    }
+                }
+
+                long joltage = long.Parse(string.Join("", joltageList));
+                answer += joltage;
             }
 
             return answer.ToString();
