38cd421e8dd9e7bad4d18195c2cacf33dda9f8e8
Austin Owensby <austin_owensby@hotmail.com>
2025-12-06 08:48:24 -0800

    2025 Day 6 part 1

--- Shared/Services/2025/Solution2025_06Service.cs
+++ Shared/Services/2025/Solution2025_06Service.cs
@@ -8,11 +8,42 @@ namespace AdventOfCode.Services
         {
             List<string> lines = Utility.GetInputLines(2025, 6, example);
 
-            int answer = 0;
+            long answer = 0;
+
+            List<List<string>> equations = [];
 
             foreach (string line in lines)
             {
+                equations.Add(line.ToList().ChunkByExclusive(c => c == ' ').Select(c => new string(c.ToArray())).ToList());
+            }
 
+            List<string> signs = equations.Last();
+            List<List<long>> equationValues = equations.Take(equations.Count - 1).Select(s => s.Select(t => long.Parse(t)).ToList()).ToList();
+            equationValues = equationValues.Pivot();
+
+            foreach (int i in equationValues.Count)
+            {
+                string sign = signs[i];
+                long result = equationValues[i][0];
+
+                if (sign == "+")
+                {
+                    for (int j = 1; j < equationValues[i].Count; j++)
+                    {
+                        long value = equationValues[i][j];
+                        result += value;
+                    }
+                }
+                else
+                {
+                    for (int j = 1; j < equationValues[i].Count; j++)
+                    {
+                        long value = equationValues[i][j];
+                        result *= value;
+                    }
+                }
+
+                answer += result;
             }
 
             return answer.ToString();
