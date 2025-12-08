2a4fc5cfb68fe1ec1445f6a29e08692a8b80f062
Austin Owensby <austin_owensby@hotmail.com>
2025-12-08 15:54:27 -0800

    2025 Day 8 part 2

--- Shared/Services/2025/Solution2025_08Service.cs
+++ Shared/Services/2025/Solution2025_08Service.cs
@@ -99,12 +99,96 @@ namespace AdventOfCode.Services
         public string SecondHalf(bool example)
         {
             List<string> lines = Utility.GetInputLines(2025, 8, example);
+            List<(int x, int y, int z)> points = lines.Select(l => l.Split(",")).Select(p => (int.Parse(p[0]), int.Parse(p[1]), int.Parse(p[2]))).ToList();
 
+            Dictionary<(int, int), double> distances = [];
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                (int x1, int y1, int z1) = points[i];
+
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    (int x2, int y2, int z2) = points[j];
+
+                    int x = x1 - x2;
+                    int y = y1 - y2;
+                    int z = z1 - z2;
+
+                    double distance = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+
+                    distances[(i,j)] = distance;
+                }
+            }
+
+            List<(int, int)> connections = distances.OrderBy(d => d.Value).Select(x => x.Key).ToList();
+
+            List<List<int>> circuits = [];
             int answer = 0;
 
-            foreach (string line in lines)
+            while (answer == 0)
             {
+                foreach ((int a, int b) in connections)
+                {
+                    List<List<int>> matchingCircuits = [];
 
+                    bool alreadyInCircuit = false;
+                    foreach (List<int> circuit in circuits)
+                    {
+                        bool aMatch = circuit.Contains(a);
+                        bool bMatch = circuit.Contains(b);
+
+                        if (aMatch && bMatch)
+                        {
+                            alreadyInCircuit = true;
+                            break;
+                        }
+                        else if (aMatch || bMatch)
+                        {
+                            matchingCircuits.Add(circuit);
+                        }
+                    }
+
+                    if (alreadyInCircuit)
+                    {
+                        continue;
+                    }
+
+                    if (matchingCircuits.Count == 0)
+                    {
+                        // Create new circuit
+                        circuits.Add([a, b]);
+                    }
+                    else if (matchingCircuits.Count == 1)
+                    {
+                        // Add to existing circuit
+                        if (matchingCircuits[0].Contains(a))
+                        {
+                            matchingCircuits[0].Add(b);
+                        }
+                        else
+                        {
+                            matchingCircuits[0].Add(a);
+                        }
+                    }
+                    else
+                    {
+                        // Merge multiple circuits
+                        List<int> newCircuit = [];
+                        foreach (List<int> circuit in matchingCircuits)
+                        {
+                            circuits.Remove(circuit);
+                            newCircuit.AddRange(circuit);
+                        }
+                        circuits.Add(newCircuit);
+                    }
+
+                    if (lines.Count == circuits.First().Count)
+                    {
+                        answer = points[a].x * points[b].x;
+                        break;
+                    }         
+                }
             }
 
             return answer.ToString();
