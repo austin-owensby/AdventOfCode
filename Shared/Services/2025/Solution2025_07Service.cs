cdd03cb2bbd403683e25bd3b0fc559f7bff0bd7f
Austin Owensby <austin_owensby@hotmail.com>
2025-12-08 15:45:04 -0800

    2025 day 7 part 2

--- Shared/Services/2025/Solution2025_07Service.cs
+++ Shared/Services/2025/Solution2025_07Service.cs
@@ -10,24 +10,92 @@ namespace AdventOfCode.Services
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            Queue<Point> queue = [];
 
+            int x = lines[0].IndexOf('S');
+            Point start = new(x, 0);
+            queue.Enqueue(start);
+
+            List<string> visitedPoints = [$"{start.X},{start.Y}"];
+
+            while (queue.Count > 0)
+            {
+                Point point = queue.Dequeue();
+
+                if (lines[point.Y][point.X] == '^')
+                {
+                    answer++;
+                    
+                    // Split
+                    Point nextPoint = new(point.X - 1, point.Y);
+                    if (!visitedPoints.Contains($"{nextPoint.X},{nextPoint.Y}"))
+                    {
+                        queue.Enqueue(nextPoint);
+                        visitedPoints.Add($"{nextPoint.X},{nextPoint.Y}");
+                    }
+
+                    nextPoint = new(point.X + 1, point.Y);
+                    if (!visitedPoints.Contains($"{nextPoint.X},{nextPoint.Y}"))
+                    {
+                        queue.Enqueue(nextPoint);
+                        visitedPoints.Add($"{nextPoint.X},{nextPoint.Y}");
+                    }
+                }
+                else
+                {
+                    // Move down
+                    Point nextPoint = new(point.X, point.Y + 1);
+                    if (point.Y + 1 != lines.Count && !visitedPoints.Contains($"{nextPoint.X},{nextPoint.Y}"))
+                    {
+                        visitedPoints.Add($"{nextPoint.X},{nextPoint.Y}");
+                        queue.Enqueue(nextPoint);
+                    }
+                }
             }
 
             return answer.ToString();
         }
 
+        private Dictionary<string, long> paths {get; set;} = [];
+        private List<string> grid { get; set; } = [];
+
+        private long GetPaths(int x, int y)
+        {
+            string key = $"{x},{y}";
+
+            if (!paths.TryGetValue(key, out long pathCount))
+            {
+                if (grid[y][x] == '^')
+                {
+                    // Split
+                    pathCount = GetPaths(x - 1, y) + GetPaths(x + 1, y);
+                }
+                else
+                {
+                    // Move down
+                    if (y + 1 != grid.Count)
+                    {
+                        pathCount = GetPaths(x, y + 1);
+                    }
+                    else
+                    {
+                        pathCount = 1;
+                    }
+                }
+                paths[key] = pathCount;
+            }
+
+            return pathCount;
+        }
+
         public string SecondHalf(bool example)
         {
-            List<string> lines = Utility.GetInputLines(2025, 7, example);
+            grid = Utility.GetInputLines(2025, 7, example);
+            paths = [];
 
-            int answer = 0;
+            int x = grid[0].IndexOf('S');
 
-            foreach (string line in lines)
-            {
-
-            }
+            long answer = GetPaths(x, 0);
 
             return answer.ToString();
         }
